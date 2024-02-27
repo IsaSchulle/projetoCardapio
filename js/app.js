f2b80bcf9535c3d25ecceafa3fa29192e61df0f0
IsaSchulle <isaschulle.dev@gmail.com>
2024-02-27 18:39:27 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+    cardapio.eventos.init();
+})
+
+var cardapio = {};
+
+cardapio.eventos = {
+    init: () => {
+        cardapio.metodos.obterItensCardapio();
+    }
+}
+
+cardapio.metodos = {
+    //obtem a lista de itens do cardapio
+    obterItensCardapio: (categoria = 'burgers') => {
+        var filtro = MENU[categoria];
+        console.log(filtro);
+
+        $("#itensCardapio").html('')
+
+        $.each(filtro, (i, e) => {
+            console.log(e.name);
+            let temp = cardapio.templates.item.replace(/\${img}/g, e.img)
+            .replace(/\${name}/g, e.name)
+            .replace(/\${price}/g, e.price.toFixed(2).replace('.', ','));
+
+            $("#itensCardapio").append(temp);
+        })
+    },
+}
+
+cardapio.templates = {
+    item: `
+        <div class="col-3 mb-5">
+            <div class="card card-item">
+                <div class="img-produto">
+                    <img src="\${img}">
+                </div>
+                <p class="title-produto text-center mt-4">
+                    <b>\${name}</b>
+                </p>
+                <p class="price-produto text-center">
+                    <b>R$ \${price}</b>
+                </p>
+                <div class="add-carrinho">
+                    <span class="btn-menos">
+                        <i class="fas fa-minus"></i>
+                    </span>
+                    <span class="add-numero-itens">
+                        0
+                    </span>
+                    <span class="btn-mais">
+                        <i class="fas fa-plus"></i>
+                    </span>
+                    <span class="btn btn-add">
+                        <i class="fa fa-cart-plus" aria-hidden="true"></i>
+                    </span>
+                </div>
+            </div>
+        </div>
+    `
+}
+
+// Faltando 5 minutos
